15a351f5854426b78535c965bfdc297f4ad31427
alicana <alican.albayrak@comodo.com>
2018-05-08 10:39:14 +0300

    initial commit

---
+++ src/main/java/com/crealytics/adverts/reportingservice/CrealyticsAdvertsReportingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.crealytics.adverts.reportingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrealyticsAdvertsReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrealyticsAdvertsReportingServiceApplication.class, args);
+	}
+}
