371ea0213c3f78b155a7cdfc2f96aa62ceb0d8ff
alicana <albayrakalican@gmail.com>
2018-05-10 01:35:53 +0300

    metrics entity added.

---
+++ src/main/java/com/crealytics/adverts/reportingservice/domain/ReportMetrics.java
@@ -0,0 +1,38 @@
+package com.crealytics.adverts.reportingservice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author alican.albayrak
+ */
+@Getter
+@Setter
+@Entity(name = "ReportMetrics")
+public class ReportMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "ctr")
+    private Double clickThroughRate;
+
+    @NotNull
+    @Column(name = "cr")
+    private Double conversionRate;
+
+    @NotNull
+    @Column(name = "fr")
+    private Double fillRate;
+
+    @NotNull
+    @Column(name = "ecpm")
+    private Double effectieCostPerThousand;
+
+
+}
